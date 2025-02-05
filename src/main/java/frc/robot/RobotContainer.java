dc3425a29fddc1a9c81c386d67f52c4115422e07
Nexi♡ <98440899+NexoShez@users.noreply.github.com>
2025-02-04 16:51:43 -0800

    add constants and stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,49 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drive;
 
 public class RobotContainer {
-  public RobotContainer() {
+  private final Drive mDrive;
+  
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(0);
+
+  public RobotContainer() { // 15, 2,14,0
+    TalonSRX driveFL = new TalonSRX(15);
+    TalonSRX driveFR = new TalonSRX(2);
+    TalonSRX driveBL = new TalonSRX(14);
+    TalonSRX driveBR = new TalonSRX(10);
+
+    // settings
+
+    driveFL.setInverted(false);
+    driveFR.setInverted(false);
+    driveBL.setInverted(false);
+    driveBR.setInverted(false);
+
+    driveFL.configPeakCurrentLimit(40);
+    driveFR.configPeakCurrentLimit(40);
+    driveBL.configPeakCurrentLimit(40);
+    driveBR.configPeakCurrentLimit(40);
+
+    mDrive = new Drive(driveFL, driveFR, driveBL, driveBR);
+
+    mDrive.setDefaultCommand(
+      new RunCommand(() -> mDrive.SetSpeed(
+        m_driverController.getLeftY(),
+        m_driverController.getRightY()
+        ),
+         mDrive
+      )
+    );
+
     configureBindings();
   }
 
--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,20 +4,43 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
   private final TalonSRX mFrontLeft;
+  private final TalonSRX mFrontRight;
+  private final TalonSRX mBackLeft;
+  private final TalonSRX mBackRight;
+
   /** Creates a new Drive. */
-  public Drive(TalonSRX FL) {
+  public Drive(TalonSRX FL, TalonSRX FR,TalonSRX BL, TalonSRX BR) {
     this.mFrontLeft=FL;
+    this.mFrontRight=FR;
+    this.mBackLeft=BL;
+    this.mBackRight=BR;
   }
 
   //HELP ME I CANT TEACH :c
 
-  
+  public void SetSpeed(double left, double right) {
+    // neveruse "camelcase" fr
+
+    /*
+     * integers = whole number NO DEdCIMELSMMS
+     * 
+     * doubles = number with or not with decimals
+     */
+
+     left=left*-1;
+
+    mFrontLeft.set(TalonSRXControlMode.PercentOutput, left);
+    mBackLeft.set(TalonSRXControlMode.PercentOutput, left);
+    mFrontRight.set(TalonSRXControlMode.PercentOutput, right);
+    mBackRight.set(TalonSRXControlMode.PercentOutput, right);
+  }
 
   @Override
   public void periodic() {
